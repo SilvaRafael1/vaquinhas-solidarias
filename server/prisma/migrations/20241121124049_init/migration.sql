d9214328a70f290f3a398465bfd563ab027cbcd4
SilvaRafael1 <rafaelaugustodasilva2@gmail.com>
2024-11-21 11:34:12 -0300

    add prisma orm

---
+++ server/prisma/migrations/20241121124049_init/migration.sql
@@ -0,0 +1,8 @@
+-- CreateTable
+CREATE TABLE "Vaquinha" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "name" TEXT NOT NULL,
+    "desc" TEXT NOT NULL,
+    "url" TEXT NOT NULL,
+    "image" TEXT
+);
