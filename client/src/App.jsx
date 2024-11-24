21020708160fd5e2d5deec9c6e99d6ae695e4b3c
Rafael Augusto da Silva <rafaelaugustodasilva2@gmail.com>
2024-11-24 18:37:21 -0300

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,22 @@
 import { useEffect, useState } from "react"
 import client from "./api/Api"
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import { CardActions } from "@mui/material";
-
+import { 
+  Button,
+  Card,
+  CardContent,
+  CardMedia,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+  TextField
+} from "@mui/material";
 
 export default function App() {
   const [vaquinhas, setVaquinhas] = useState([])
+  const [openDialog, setOpenDialog] = useState(false);
+  const [error, setError] = useState("");
   
   const listVaquinhas = async () => {
     try {
@@ -26,41 +35,116 @@ export default function App() {
     listVaquinhas();
   }, [])
 
+  const handleDialog = (event) => {
+    event.preventDefault();
+    setOpenDialog(true)
+  }
+
+  const handleDialogClose = () => {
+    setOpenDialog(false)
+  }
+  
   return (
-    <div className="flex flex-col items-center justify-center bg-orange-100 h-screen">
+    <div className="flex flex-col items-center justify-center bg-zinc-200 min-h-screen">
       <div className="text-4xl font-serif font-semibold text-blue-700 mt-4">Vaquinhas Solidárias</div>
       <div className="text-2xl font-sans text-blue-600 mb-4">Sua doação pode salvar uma vida!</div>
       <div className="border-2 border-blue-600 border-solid rounded-md p-4 w-[1000px] bg-white">
-        <Card className="flex my-2">
-          <CardMedia
-            component="img"
-            sx={{ width: 151 }}
-            alt="ABC"
-            image="https://employer.com.br/wp-content/uploads/2022/01/empurre-sua-vaquinha.jpg"
-          />
-          <CardContent>
-            ABC
-          </CardContent>
-          <CardActions>
-            LINK DA VAQUINHA
-          </CardActions>
-        </Card>
-        <Card sx={{ display: 'flex' }}>
-          <CardMedia
-            component="img"
-            sx={{ width: 151 }}
-            alt="ABC"
-            image="https://employer.com.br/wp-content/uploads/2022/01/empurre-sua-vaquinha.jpg"
-          />
-          <CardContent>
-            ABC
-          </CardContent>
-          <CardActions>
-            LINK DA VAQUINHA
-          </CardActions>
-        </Card>
+        {vaquinhas.map((vaquinha) => (
+          <Card className="flex my-2 justify-between items-center" key={vaquinha.id}>
+            <CardMedia 
+              component="img"
+              className="h-32"
+              alt={vaquinha.name}
+              image={vaquinha.image}
+            />
+            <CardContent className="flex flex-col">
+              <b className="flex justify-center">
+                {vaquinha.name}
+              </b>
+              {vaquinha.desc}
+              <span className="flex justify-center mt-2">
+                <Button href={vaquinha.url} target="_blank" rel="noopener noreferrer" variant="contained">LINK DA VAQUINHA</Button>
+              </span>
+            </CardContent>
+          </Card>
+        ))}
       </div>
-      <div className="text-1xl font-sans hover:text-blue-600 cursor-pointer mt-4">Quer adicionar uma vaquinha? Clique aqui!</div>
+      <div onClick={handleDialog} className="text-1xl font-sans hover:text-blue-600 cursor-pointer my-4">
+        Quer adicionar uma vaquinha? Clique aqui!
+      </div>
+
+      <Dialog
+        open={openDialog}
+        onClose={handleDialogClose}
+        PaperProps={{
+          component: 'form',
+          onSubmit: (event) => {
+            event.preventDefault();
+            const formData = new FormData(event.currentTarget);
+            const formJson = Object.fromEntries(formData.entries());
+            client.post(`/vaquinhas`, formJson).then(() => {
+              handleDialogClose();
+              location.reload();
+            })
+          }
+        }}
+      >
+        <DialogTitle>Adicionar Vaquinha</DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            <span className='text-red-500'>
+              {error ? error : ""}
+            </span>
+          </DialogContentText>
+          <TextField
+            autoFocus
+            required
+            margin="dense"
+            id="name"
+            name="name"
+            label="Nome da Vaquinha"
+            type="text"
+            fullWidth
+            variant="standard"
+          />
+          <TextField
+            autoFocus
+            required
+            margin="dense"
+            id="desc"
+            name="desc"
+            label="Descrição da Vaquinha"
+            type="text"
+            fullWidth
+            variant="standard"
+          />
+          <TextField
+            autoFocus
+            required
+            margin="dense"
+            id="url"
+            name="url"
+            label="Link da Vaquinha"
+            type="text"
+            fullWidth
+            variant="standard"
+          />
+          <TextField
+            autoFocu
+            margin="dense"
+            id="image"
+            name="image"
+            label="URL da imagem"
+            type="text"
+            fullWidth
+            variant="standard"
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleDialogClose}>Cancelar</Button>
+          <Button type='submit' variant="contained">Adicionar</Button>
+        </DialogActions>
+      </Dialog>
     </div>
   )
 }
